35cb20ca2abccdd4742f377986867fa0d2f91fe3
Leejeongchan1733026 <cksqls288@gmail.com>
2022-07-16 20:40:03 +0900

    add cell item list

---
+++ Button Click Action CollectionView/Button Click Action CollectionView/productList.swift
@@ -0,0 +1,14 @@
+//
+//  productList.swift
+//  Button Click Action CollectionView
+//
+//  Created by 이정찬 on 2022/07/16.
+//
+
+import Foundation
+import UIKit
+struct productList
+{
+    let productImage: UIImage
+    let porductName: String
+}
